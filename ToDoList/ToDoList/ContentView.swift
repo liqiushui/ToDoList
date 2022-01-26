391cf56bb55ef9c7c78cef32ee4cc38f4974bcbf
lilun.ios <lilun.ios@bytedance.com>
2022-01-26 10:52:37 +0800

    init empty project

---
+++ ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by lilun.ios on 2022/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by lilun.ios on 2022/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
